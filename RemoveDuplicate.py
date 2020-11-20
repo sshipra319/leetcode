135a44cb00e058995e16218e712cf94e4740ff98
sshipra319 <56846583+sshipra319@users.noreply.github.com>
2020-11-20 16:40:00 -0600

    Add files via upload

---
+++ RemoveDuplicate.py
@@ -0,0 +1,16 @@
+class Solution:
+    def deleteDuplicates(self, head):
+        if head == None:
+            return head
+        curr = head.next
+        prev = head
+  
+        while curr != None:
+            if curr.val == prev.val:
+                prev.next = curr.next
+                curr = curr.next
+            else:
+                curr = curr.next
+                prev = prev.next
+    
+        return head
