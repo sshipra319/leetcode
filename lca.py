653439fffe97560781bf830bad505f2c6c6f3829
sshipra319 <56846583+sshipra319@users.noreply.github.com>
2020-11-23 15:05:52 -0600

    Add files via upload

---
+++ lca.py
@@ -0,0 +1,11 @@
+def lca(root, v1, v2):
+    
+    if root is None:
+        return root
+        
+    if (root.info > v1 and root.info > v2):
+        return lca(root.left, v1, v2)
+    if (root.info < v1 and root.info < v2):
+        return lca(root.right, v1, v2)
+    
+    return root
