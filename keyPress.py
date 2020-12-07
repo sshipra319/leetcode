9c69688a2a7517e8217530f2a11876cfb9e43c1b
sshipra319 <56846583+sshipra319@users.noreply.github.com>
2020-12-07 11:21:17 -0600

    Add files via upload

---
+++ keyPress.py
@@ -0,0 +1,19 @@
+def slowestKey(keyTimes):
+    keyTimes = [[chr(k[0] + 97), k[1]] for k in keyTimes]                    
+    longest_key = None
+    longest_duration = None    
+    for i in range(len(keyTimes)):
+        if i == 0:
+            start = 0
+        else:
+            start = keyTimes[i - 1][1]
+        end = keyTimes[i][1]
+        char = keyTimes[i][0]
+        interval = end - start
+        if not longest_duration or interval > longest_duration:
+            longest_duration = interval
+            longest_key = char
+    return longest_key
+    
+output1 = slowestKey([[0, 1], [0, 3], [4, 5], [5, 6], [4,10]])
+print(output1)
